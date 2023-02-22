385a087f775231d20f3157adabfeebd388763483
Sayemhaque <mdsayemofficial1000@gmail.com>
2023-02-21 22:22:29 -0800

    knowing about spread operator

---
+++ spreadOperator.js
@@ -0,0 +1,7 @@
+const fruits = ["mangos", "guava", "banana", "apple"];
+
+// spreading old array into an the new array and adding some new elements;
+const newFruits = [...fruits, "orange", "pinapple"];
+
+console.log(fruits); // output : [ 'mangos', 'guava', 'banana', 'apple' ]
+console.log(newFruits); // output : [ 'mangos', 'guava', 'banana', 'apple', 'orange', 'pinapple' ]
